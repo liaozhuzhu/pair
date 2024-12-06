42c160c19471ea59ab4998689dec6d87334ef3f1
Liao Zhu <liao.zhu.cs@gmail.com>
2024-12-06 02:21:55 -0600

    v1 endpoitns

--- server/server.py
+++ server/server.py
@@ -2,12 +2,26 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/pairapi")
 def api():
+    """
+    main default route
+    """
     return "Hello from pAIr"
 
-@app.route("/api/chat")
+
+@app.route("/pairapi/chat", methods=['POST'])
 def chat():
+    """
+    Takes in user query and returns text response
+    """
+    pass
+
+@app.route('/pairapi/upload', methods=['POST'])
+def upload():
+    """
+    handle text based file uploads
+    """
     pass
 
 if __name__ == "__main__": # so this file is not importable in other modules
